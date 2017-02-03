6c38dd74fdc6cd87c1045adec0e74e25616ef9e1
Dan Finelli <dan.finelli@hotmail.com>
2017-02-02 21:52:58 -0800

    fixing button scroll to be slow

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
   console.log("sanity check");
   $('.parallax').parallax();
+
   $(".dropdown-button").dropdown();
 
+
+  $(".work_btn").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#my_projects").offset().top
+      }, 2000);
+  });
 });
