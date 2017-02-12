1e143ff7da7b65cd2228c3e8cae1305be3f978ae
Dan Finelli <dan.finelli@hotmail.com>
2017-02-11 22:38:20 -0500

    tryna fix this form

--- Scripts/main.js
+++ Scripts/main.js
@@ -31,7 +31,9 @@ $(document).ready(function(){
   $("form").submit(function (event) {
     event.preventDefault();
     var formData = $("form").serialize();
-    console.log(formData.first_name);
+    console.log(formData);
+    console.log(formdata);
+    console.log(formData[first_name]);
     var mailto_link = 'mailto:'+email+'?subject='+first_name+'%20'+last_name+'&body='+textarea1;
     window.open(mailto_link);
     alert("Thank You!");
