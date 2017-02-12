335a676dafd3c22ac2eb117c47073b46435d0743
Dan Finelli <dan.finelli@hotmail.com>
2017-02-11 22:08:12 -0500

    fixing form opens new tab

--- Scripts/main.js
+++ Scripts/main.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
   $("form").submit(function (event) {
     event.preventDefault();
     var mailto_link = 'mailto:'+email+'?subject='+first_name+'%20'+last_name+'&body='+textarea1;
-    $(window).open = mailto_link;
+    window.open = mailto_link;
     alert("Thank You!");
     var formData = $("form").serialize();
     console.log(formData);
